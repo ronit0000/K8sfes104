1b668dacea5259f08621f00f78bf4040956664c6
Ronit Kumar Sahu <145933979+ronit0000@users.noreply.github.com>
2025-11-05 21:25:29 +0530

    Change base path for K8s deployment
    Update base path for Kubernetes deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// Correct base path for K8s deployment
 export default defineConfig({
   plugins: [react()],
-  base: '/ecommerce-frontend/',
+  base: '/ecommerce/',   // ✅ must match your ingress or NodePort route
 })
