5554a458a88bdeaddaea44d787296c01d8a5ab79
Ronit Kumar Sahu <145933979+ronit0000@users.noreply.github.com>
2025-11-05 21:02:23 +0530

    Update API_URL to new authentication endpoint

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://3.82.176.213:9090/back1/auth";
+const API_URL = "http://ec2-35-173-125-222.compute-1.amazonaws.com:9090/back1/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
